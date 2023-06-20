7ee491fb631ae6af455a6d042aef89db7560d0f4
ayman <aymanzaman23@gmail.com>
2023-06-20 17:08:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+
 
 function App() {
+  // Initializing the state
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // Since we are in a fetching process, so setLoading = true
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      //Data has been fetched so turn setLoading back to false becasue API calling is done 
+      setLoading(false);
+    };
+
+    fetchPosts()
+  }, [])
+
+  
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost)
+
+  //Change Page
+  const paginate = (pageNumber)=> setCurrentPage(pageNumber)
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} 
+      loading = {loading}/>
+      <Pagination 
+      postsPerPage={postsPerPage} 
+      totalPosts={posts.length}
+      paginate={paginate}/>
     </div>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Pagination = ({postsPerPage,totalPosts,paginate}) => {
+
+  const pageNumbers =[]
+  
+  //to get correct amount of page numbers
+  for (let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++ ){
+    pageNumbers.push(i)
+  }
+
+
+
+
+  return (
+    <nav>
+        <ul className='pagination'>
+            {pageNumbers.map(number =>(
+                <li key={number} 
+                className = 'page-item'>
+                    <a href="!#" 
+                    className='page-link'
+                    onClick={()=>paginate(number)}>
+                        {number}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </nav>
+  )
+}
+
+export default Pagination
